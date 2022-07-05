f5d0ea4bea9fb6c66faf22edd2a86befe0ba203d
Sebastián <sebastian.contactoweb@gmail.com>
2022-07-04 19:36:51 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function giveAdvice(){
+    const adviceArea = document.querySelector(".advice");
+
+    function getAdvice() {
+      fetch("https://api.adviceslip.com/advice")
+        .then((res) => res.json())
+        .then(
+          (data) =>
+            (adviceArea.innerHTML = `<span class="id">${data.slip.id}.</span><h2 class="advice">${data.slip.advice}</h2>`)
+        );
+    }
+    
+    getAdvice();
+}
